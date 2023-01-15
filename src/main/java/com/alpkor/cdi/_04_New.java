36d108df2dda9215a42e250ab1029e1a8b01cb37
alperkorkmaz <dr.alperkorkmaz@gmail.com>
2023-01-15 22:03:05 +0100

    @Produces @Inject @New annotations were added

---
+++ src/main/java/com/alpkor/cdi/_04_New.java
@@ -0,0 +1,27 @@
+package com.alpkor.cdi;
+
+import com.alpkor.StudentDto;
+import lombok.Getter;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.inject.New;
+import javax.enterprise.inject.Produces;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+@Named(value="newtuto")
+@ApplicationScoped
+public class _04_New {
+    @Produces
+    public String uretilenData(@New StudentDto studentDto){
+        studentDto=StudentDto
+                    .builder()
+                    .studentId(0L)
+                    .studentName("Student Adi")
+                    .build();
+        return studentDto.getStudentName();
+    }
+    @Getter    @Inject
+    private String tuketilenData;
+
+}
